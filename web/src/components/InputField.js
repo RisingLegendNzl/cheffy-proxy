0f214fd84301c3ed4d76b890dfd67737fb5024d4
LEGEND <risinglegendnzl@gmail.com>
2025-11-04 12:49:14 +1000

    Create InputField.js

---
+++ web/src/components/InputField.js
@@ -0,0 +1,9 @@
+// web/src/components/InputField.js
+// TODO: Import React.
+
+export default function InputField() {
+  // TODO: Migrate InputField component logic [cite: 46-54]
+  return null;
+}
+
+
