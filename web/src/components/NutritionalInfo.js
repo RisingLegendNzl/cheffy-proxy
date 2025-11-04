ee79c75ccfc3f91e4b900b0f227b810add23d049
LEGEND <risinglegendnzl@gmail.com>
2025-11-04 12:51:17 +1000

    Add NutritionalInfo component with initial structure
    Create NutritionalInfo component skeleton with TODOs.

---
+++ web/src/components/NutritionalInfo.js
@@ -0,0 +1,9 @@
+// web/src/components/NutritionalInfo.js
+// TODO: Import React and lucide-react icons.
+
+export default function NutritionalInfo() {
+  // TODO: Migrate NutritionalInfo component logic [cite: 228-248]
+  return null;
+}
+
+
