cdcf50be08855d57442a2faeb37b6b4604785bcc
LEGEND <risinglegendnzl@gmail.com>
2025-11-04 12:48:20 +1000

    Create GenerationProgressDisplay.js

---
+++ web/src/components/GenerationProgressDisplay.js
@@ -0,0 +1,9 @@
+// web/src/components/GenerationProgressDisplay.js
+// TODO: Import React and lucide-react icons.
+
+export default function GenerationProgressDisplay() {
+  // TODO: Migrate GenerationProgressDisplay component logic [cite: 144-227]
+  return null;
+}
+
+
