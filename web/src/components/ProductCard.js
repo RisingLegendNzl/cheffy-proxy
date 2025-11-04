01cf70666a165c3a2faaa9feca21b64f8bfdd818
LEGEND <risinglegendnzl@gmail.com>
2025-11-04 12:51:43 +1000

    Create ProductCard component with placeholders
    Initial creation of ProductCard component with placeholders.

---
+++ web/src/components/ProductCard.js
@@ -0,0 +1,9 @@
+// web/src/components/ProductCard.js
+// TODO: Import React and lucide-react icons.
+
+export default function ProductCard() {
+  // TODO: Migrate ProductCard component logic [cite: 81-109]
+  return null;
+}
+
+
