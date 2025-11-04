064671aac9abf982ce29b1b43921bc426cd8d8c0
LEGEND <risinglegendnzl@gmail.com>
2025-11-04 12:52:39 +1000

    Create SubstituteMenu.js

---
+++ web/src/components/SubstituteMenu.js
@@ -0,0 +1,9 @@
+// web/src/components/SubstituteMenu.js
+// TODO: Import React, useState, and lucide-react icons.
+
+export default function SubstituteMenu() {
+  // TODO: Migrate SubstituteMenu component logic [cite: 129-143]
+  return null;
+}
+
+
