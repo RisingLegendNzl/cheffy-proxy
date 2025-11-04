39aa42d926ce5d5a8d234d5ddac607bbd593fad5
LEGEND <risinglegendnzl@gmail.com>
2025-11-04 12:47:00 +1000

    Create DiagnosticLogViewer.js

---
+++ web/src/components/DiagnosticLogViewer.js
@@ -0,0 +1,10 @@
+// web/src/components/DiagnosticLogViewer.js
+// TODO: Import React, hooks, and lucide-react icons.
+// TODO: Import LogEntry component.
+
+export default function DiagnosticLogViewer() {
+  // TODO: Migrate DiagnosticLogViewer component logic [cite: 383-441]
+  return null;
+}
+
+
