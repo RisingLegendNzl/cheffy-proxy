bc29b4df17b0ae7bb872112382c3ae44d294b7cf
LEGEND <risinglegendnzl@gmail.com>
2025-11-04 12:45:24 +1000

    Create App.js

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+// web/src/App.js
+// TODO: Import React, hooks, components, and Firebase.
+
+export default function App() {
+  // TODO: Migrate all state, logic, and layout here from Cheffy.txt [cite: 97-550]
+  return (
+    <div>
+      <h1>Cheffy App Stub</h1>
+      {/* TODO: Add main layout */}
+    </div>
+  );
+}
+
+
