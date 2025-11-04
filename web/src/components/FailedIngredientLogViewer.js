9d4c867dc5222a4ba891b5a8a2546f793710c19e
LEGEND <risinglegendnzl@gmail.com>
2025-11-04 12:47:49 +1000

    Create FailedIngredientLogViewer.js

---
+++ web/src/components/FailedIngredientLogViewer.js
@@ -0,0 +1,9 @@
+// web/src/components/FailedIngredientLogViewer.js
+// TODO: Import React, useState, and lucide-react icons.
+
+export default function FailedIngredientLogViewer() {
+  // TODO: Migrate FailedIngredientLogViewer component logic [cite: 442-476]
+  return null;
+}
+
+
