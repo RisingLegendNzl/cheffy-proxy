14b2010b22b88db4183cf24e879dc356e6fb9d5e
LEGEND <risinglegendnzl@gmail.com>
2025-11-04 12:52:20 +1000

    Create RecipeModal.js

---
+++ web/src/components/RecipeModal.js
@@ -0,0 +1,8 @@
+// web/src/components/RecipeModal.js
+// TODO: Import React and lucide-react icons.
+
+export default function RecipeModal() {
+  // TODO: Migrate RecipeModal component logic [cite: 477-518]
+  return null;
+}
+
