cc60ef619eb7283fc253a857a56b4eac0016f18b
LEGEND <risinglegendnzl@gmail.com>
2025-11-04 14:15:11 +1000

    Update index.jsx

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,7 +1,7 @@
 // web/src/index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 
 // Note: We are not importing a global CSS file
 // Styling is handled by Tailwind CDN linked in index.html
@@ -14,4 +14,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
