fb2623461d6096ab9f7fa8a8961a5c528168612a
LEGEND <risinglegendnzl@gmail.com>
2025-11-04 12:45:50 +1000

    Create CollapsibleSection.js

---
+++ web/src/components/CollapsibleSection.js
@@ -0,0 +1,9 @@
+// web/src/components/CollapsibleSection.js
+// TODO: Import React, useState, and lucide-react icons.
+
+export default function CollapsibleSection() {
+  // TODO: Migrate CollapsibleSection component logic [cite: 110-128]
+  return null;
+}
+
+
