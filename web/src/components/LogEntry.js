cfce346c3786787b4b885f3d760e92777abd739f
LEGEND <risinglegendnzl@gmail.com>
2025-11-04 12:49:33 +1000

    Create LogEntry.js

---
+++ web/src/components/LogEntry.js
@@ -0,0 +1,9 @@
+// web/src/components/LogEntry.js
+// TODO: Import React, useState, and lucide-react icons.
+
+export default function LogEntry() {
+  // TODO: Migrate LogEntry component logic [cite: 369-382]
+  return null;
+}
+
+
