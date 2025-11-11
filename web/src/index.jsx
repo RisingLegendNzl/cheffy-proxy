ecf01cf697183c45ace5ab12596f339767308dd3
LEGEND <risinglegendnzl@gmail.com>
2025-11-12 07:23:18 +1000

    Update index.jsx

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,19 +1,13 @@
 // web/src/index.jsx
-import './animations.css';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-// Note: We are not importing a global CSS file
-// Styling is handled by Tailwind CDN linked in index.html
-
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
-// [FIX] Removed React.StrictMode
-// This was causing the SSE fetch stream to be double-invoked in development,
-// leading to a "Body is disturbed or locked" error.
+// Removed React.StrictMode to prevent double SSE fetch
 root.render(
     <App />
 );
-
