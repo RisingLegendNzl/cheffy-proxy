59dc4969c8ae97957c36b9ba3bb79402ec8750a4
LEGEND <risinglegendnzl@gmail.com>
2025-11-12 22:16:47 +1000

    Update DayNavigator.jsx

--- web/src/components/DayNavigator.jsx
+++ web/src/components/DayNavigator.jsx
@@ -1,28 +1,27 @@
 // web/src/components/DayNavigator.jsx
-import React from ‘react’;
-import WeekHorizonSelector from ‘./day-selector/WeekHorizonSelector’;
+import React from 'react';
+import WeekHorizonSelector from './day-selector/WeekHorizonSelector';
 
 /**
-
-- Day Navigator - Wrapper component for day selection
-- Now uses the Week Horizon concept for premium UX
-  */
-  const DayNavigator = ({
-  selectedDay,
-  totalDays,
-  onDayChange,
-  eatenMeals = {},
-  mealPlan = [],
-  }) => {
-  return (
-  <WeekHorizonSelector
-selectedDay={selectedDay}
-totalDays={totalDays}
-onDayChange={onDayChange}
-eatenMeals={eatenMeals}
-mealPlan={mealPlan}
-/>
-  );
-  };
+ * Day Navigator - Wrapper component for day selection
+ * Now uses the Week Horizon concept for premium UX
+ */
+const DayNavigator = ({
+    selectedDay,
+    totalDays,
+    onDayChange,
+    eatenMeals = {},
+    mealPlan = [],
+}) => {
+    return (
+        <WeekHorizonSelector
+            selectedDay={selectedDay}
+            totalDays={totalDays}
+            onDayChange={onDayChange}
+            eatenMeals={eatenMeals}
+            mealPlan={mealPlan}
+        />
+    );
+};
 
 export default DayNavigator;
