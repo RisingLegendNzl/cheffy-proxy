3cedcf1b3a3926a4d0d430305a7abd184c51e2fc
LEGEND <risinglegendnzl@gmail.com>
2025-11-12 23:00:19 +1000

    Update AmbientOverlay.jsx

--- web/src/components/ui/AmbientOverlay.jsx
+++ web/src/components/ui/AmbientOverlay.jsx
@@ -1,44 +1,40 @@
 // web/src/components/ui/AmbientOverlay.jsx
-import React, { useEffect, useState } from ‘react’;
-import ParticleBackground from ‘../animations/ParticleBackground’;
-import { getTimeOfDay } from ‘../../utils/animationHelpers’;
+import React, { useEffect, useState } from 'react';
+import ParticleBackground from '../animations/ParticleBackground';
+import { getTimeOfDay } from '../../utils/animationHelpers';
 
 /**
-
-- Ambient Overlay - Time-of-day gradients and particle effects
-- Features:
-- - Manages time-of-day gradients
-- - Ambient particle effects
-- - Smooth transitions between times
-- - Non-intrusive background layer
-    */
-    const AmbientOverlay = ({
+ * Ambient Overlay - Time-of-day gradients and particle effects
+ * Features:
+ * - Manages time-of-day gradients
+ * - Ambient particle effects
+ * - Smooth transitions between times
+ * - Non-intrusive background layer
+ */
+const AmbientOverlay = ({
     enableGradient = true,
     enableParticles = true,
     particleCount = 20,
-    }) => {
-    const [timeOfDay, setTimeOfDay] = useState(‘morning’);
+}) => {
+    const [timeOfDay, setTimeOfDay] = useState('morning');
 
-useEffect(() => {
-const updateTimeOfDay = () => {
-setTimeOfDay(getTimeOfDay());
-};
+    useEffect(() => {
+        const updateTimeOfDay = () => {
+            setTimeOfDay(getTimeOfDay());
+        };
 
-```
-updateTimeOfDay();
-const interval = setInterval(updateTimeOfDay, 1800000); // 30 minutes
+        updateTimeOfDay();
+        const interval = setInterval(updateTimeOfDay, 1800000); // 30 minutes
 
-return () => clearInterval(interval);
-```
+        return () => clearInterval(interval);
+    }, []);
 
-}, []);
-
-return (
-<ParticleBackground
-particleCount={enableParticles ? particleCount : 0}
-enableGradient={enableGradient}
-/>
-);
+    return (
+        <ParticleBackground
+            particleCount={enableParticles ? particleCount : 0}
+            enableGradient={enableGradient}
+        />
+    );
 };
 
 export default AmbientOverlay;
